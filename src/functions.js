1e4ba73ff6aef641e6b81502cb8c5d82e4eaa1c5
Marisa Wyatt <risawyatt22@gmail.com>
2023-08-05 02:41:08 -0600

    Write function to get total trip cost

--- src/functions.js
+++ src/functions.js
@@ -1,28 +1,25 @@
 const filterUserTrips = (id, tripsData, destinationsData) => {
-  if(tripsData.find(trip => trip.userID === id )){
-  const userTrips = tripsData.filter((trip) => trip.userID === id);
-  const userDestinations = userTrips.map((trip) => {
-    trip.destinationName = destinationsData.find(
-      (destination) => destination.id === trip.destinationID
-    ).destination;
-    return trip;
-  });
-  const handleReduce = (acc, userTrip) => {
-    if (userTrip.status === "approved") {
-      acc.past.push(userTrip);
-    } else if (userTrip.status === "pending") {
-      acc.pending.push(userTrip);
-    }
-    return acc;
-  };
-  const initialValue = {
-    pending: [],
-    past: [],
-  };
-  const variable = userDestinations.reduce(handleReduce, initialValue);
-  return variable;
-  }else{
-    return `User ${id} cannot be found`
+  if (tripsData.find((trip) => trip.userID === id)) {
+    const userTrips = tripsData.filter((trip) => trip.userID === id);
+    const userDestinations = userTrips.map((trip) => {
+      trip.destinationName = destinationsData.find(
+        (destination) => destination.id === trip.destinationID
+      ).destination;
+      return trip;
+    });
+    return userDestinations.reduce((acc, userTrip) => {
+      if (userTrip.status === "approved") {
+        acc.past.push(userTrip);
+      } else if (userTrip.status === "pending") {
+        acc.pending.push(userTrip);
+      }
+      return acc;
+    }, {
+      pending: [],
+      past: [],
+    });
+  } else {
+    return `User ${id} cannot be found`;
   }
 };
 
@@ -33,18 +30,24 @@ const filterUserTrips = (id, tripsData, destinationsData) => {
 // "duration": 7,
 
 // Total amount I have spent on trips this year. This should be calculated from the trips data and include a travel agent’s 10% fee
-// const calulateTripsCost = () => {
-// use filterUserTrips func, iterate through all past trips
-// map? over each trip, get flight cost(from destinationsData) x travelers (tripsData)
-// duration(trips) x estimatedLodgingCostPerDay(destinations)
-// add 10% agent fee
-// return total cost of trip
-// reduce mapped array of trips costs to acc (init value of 0)
-// return acc
-
-// }
-
-export {
-  filterUserTrips,
-  // calculateTripsCost
+const calculateTripsCost = (id, tripsData, destinationsData) => {
+  const userTrips = filterUserTrips(id, tripsData, destinationsData);
+  const flightCost = userTrips.past.reduce((acc, trip) => {
+    const destinationFlightCost = destinationsData.find(
+      (destination) => destination.id === trip.destinationID
+    ).estimatedFlightCostPerPerson;
+    acc += trip.travelers * destinationFlightCost;
+    return acc;
+  }, 0);
+  const lodgingCost = userTrips.past.reduce((acc, trip) => {
+    const destinationLodgingCost = destinationsData.find(
+      (destination) => destination.id === trip.destinationID
+    ).estimatedLodgingCostPerDay;
+    acc += trip.duration * destinationLodgingCost;
+    return acc;
+  }, 0);
+  const agentFee = (flightCost + lodgingCost) * 0.1;
+  return flightCost + lodgingCost + agentFee;
 };
+
+export { filterUserTrips, calculateTripsCost };
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,7 +1,7 @@
 import chai from "chai";
 const expect = chai.expect;
 
-import { filterUserTrips } from "../src/functions";
+import { filterUserTrips, calculateTripsCost } from "../src/functions";
 import sampleTrips from "../src/sample-data/trips";
 console.log("sampletrips", [sampleTrips.trips[0], sampleTrips.trips[1]]);
 
@@ -170,3 +170,117 @@ describe("filterUserTrips", () => {
     expect(filterUserTrips(51, tripsData, destinationsData)).to.deep.equal(`User 51 cannot be found`)
   })
 });
+
+describe('calculateTripsCost function', () => {
+  let tripsData;
+  let destinationsData;
+  beforeEach(() => {
+    destinationsData = [
+      {
+        id: 1,
+        destination: "Lima, Peru",
+        estimatedLodgingCostPerDay: 70,
+        estimatedFlightCostPerPerson: 400,
+        image:
+          "https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1 &ixid =eyJhcHBfaWQiOjEyMDd9 &auto =format &fit =crop &w =2089 &q =80",
+        alt: "overview of city buildings with a clear sky",
+      },
+      {
+        id: 2,
+        destination: "Stockholm, Sweden",
+        estimatedLodgingCostPerDay: 100,
+        estimatedFlightCostPerPerson: 780,
+        image:
+          "https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1 &ixid =eyJhcHBfaWQiOjEyMDd9 &auto =format &fit =crop &w =1950 &q =80",
+        alt: "city with boats on the water during the day time",
+      },
+      {
+        id: 3,
+        destination: "Sydney, Austrailia",
+        estimatedLodgingCostPerDay: 130,
+        estimatedFlightCostPerPerson: 950,
+        image:
+          "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1 &ixid =eyJhcHBfaWQiOjEyMDd9 &auto =format &fit =crop &w =1950 &q =80",
+        alt: "opera house and city buildings on the water with boats",
+      },
+      {
+        id: 4,
+        destination: "Cartagena, Colombia",
+        estimatedLodgingCostPerDay: 65,
+        estimatedFlightCostPerPerson: 350,
+        image:
+          "https://images.unsplash.com/photo-1558029697-a7ed1a4b94c0?ixlib=rb-1.2.1 &ixid =eyJhcHBfaWQiOjEyMDd9 &auto =format &fit =crop &w =1650 &q =80",
+        alt: "boats at a dock during the day time",
+      },
+      {
+        id: 5,
+        destination: "Madrid, Spain",
+        estimatedLodgingCostPerDay: 150,
+        estimatedFlightCostPerPerson: 650,
+        image:
+          "https://images.unsplash.com/photo-1543785734-4b6e564642f8?ixlib=rb-1.2.1 &ixid =eyJhcHBfaWQiOjEyMDd9 &auto =format &fit =crop &w =1950 &q =80",
+        alt: "city with clear skys and a road in the day time",
+      },
+    ];
+    tripsData = [
+      {
+        id: 1,
+        userID: 1,
+        destinationID: 1,
+        travelers: 1,
+        date: "2022/09/16",
+        duration: 8,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 2,
+        userID: 2,
+        destinationID: 5,
+        travelers: 5,
+        date: "2022/10/04",
+        duration: 18,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 3,
+        userID: 1,
+        destinationID: 2,
+        travelers: 4,
+        date: "2022/05/22",
+        duration: 17,
+        status: "pending",
+        suggestedActivities: [],
+      },
+      {
+        id: 4,
+        userID: 1,
+        destinationID: 3,
+        travelers: 2,
+        date: "2022/02/25",
+        duration: 10,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 5,
+        userID: 1,
+        destinationID: 4,
+        travelers: 3,
+        date: "2022/04/30",
+        duration: 18,
+        status: "approved",
+        suggestedActivities: [],
+      },
+    ];
+  });
+
+  it('should be a function', () => {
+    expect(calculateTripsCost).to.be.a('function')
+  })
+
+  it('should return total cost plus agent fee', () => {
+    expect(calculateTripsCost(1, tripsData, destinationsData)).to.equal(7018)
+  })
+})
